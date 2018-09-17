f9bf98488a6432fa7ce3f2ffbce5c32f2566a902
冉晴 <qingr@uw.edu>
2018-09-16 17:28:05 -0700

    Initial Commit

---
+++ movie/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  movie
+//
+//  Created by Qing Ran on 9/16/18.
+//  Copyright © 2018 Qing Ran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
